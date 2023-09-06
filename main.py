c924449d5dc90519248be2f02c5277e786e3d140
gj2071 <143042340+gj2071@users.noreply.github.com>
2023-09-06 20:35:27 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+#LISTA COM DADOS DIVERSOS 
+
+list2 = [1,"ola","r", 100.5, True]
+print(list2)
+
+
+#REMOVENDO DADO DA LISTA 
+list3 =  [1,6,978]
+list3.remove(6)
+print(list3)
+
+
+#DELETANDO  INDICE DA LISTA
+list4 = [4,6,8,78]
+del list4[0]
+print(list4)
+
+
+#LEN PARA LISTAR
+
+list5 = [45,5,9,78]
+print(len(list5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
